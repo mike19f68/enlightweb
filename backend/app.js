8ae4ca583c5d9f6bfbfa6f3d7676ee4fc766e8ab
Mike Farrant <mfarrant@live.co.uk>
2021-08-27 20:35:55 +0100

    eek
    Signed-off-by: Mike Farrant <mfarrant@live.co.uk>

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const path = require("path");
 const mongoose = require('mongoose');
 
 const songsRoutes = require("./routes/songs");
@@ -22,6 +23,7 @@ mongoose
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false}));
+  app.use("/",express.static(path.join(__dirname, "code")));
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -38,6 +40,9 @@ mongoose
 
 app.use("/api/songs", songsRoutes);
 app.use("/api/sets", setsRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "code", "index.html"));
+});
 
 module.exports = app;
 
