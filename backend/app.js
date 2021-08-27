3e78217083254281d6450393f70a4961ef8cae9f
Mike Farrant <mfarrant@live.co.uk>
2021-08-27 21:10:12 +0100

    27/08/21b
    Signed-off-by: Mike Farrant <mfarrant@live.co.uk>

--- backend/app.js
+++ backend/app.js
@@ -23,7 +23,7 @@ mongoose
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false}));
-  app.use("/",express.static(path.join(__dirname, "code")));
+  app.use("/",express.static(path.join(__dirname, "Enlight")));
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -41,7 +41,7 @@ mongoose
 app.use("/api/songs", songsRoutes);
 app.use("/api/sets", setsRoutes);
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "code", "index.html"));
+  res.sendFile(path.join(__dirname, "Enlight", "index.html"));
 });
 
 module.exports = app;
