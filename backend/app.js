390c7c37a9c6d6bfa6973d7a9a243dc9ace900f0
Mike Farrant <mfarrant@live.co.uk>
2021-08-27 15:03:19 +0100

    Integrated Application
    Signed-off-by: Mike Farrant <mfarrant@live.co.uk>

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
@@ -22,6 +23,7 @@ mongoose
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false}));
+  app.use("/", express.static(path.join(__dirname, "Enlight")));
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -38,6 +40,9 @@ mongoose
 
 app.use("/api/songs", songsRoutes);
 app.use("/api/sets", setsRoutes);
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname,"Enlight","index.html"));
+});
 
 module.exports = app;
 
