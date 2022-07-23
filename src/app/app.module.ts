311228c7bff3d7b9306f570de89132ec6be37ee1
Mike Farrant <mfarrant@live.co.uk>
2022-07-23 20:20:59 +0100

    bugfix
    Signed-off-by: Mike Farrant <mfarrant@live.co.uk>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { SortablejsModule } from 'ngx-sortablejs';
 
 import { LoginComponent } from './auth/login/login.component';
-import { SignupComponent } from './auth/Signup/signup.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { BuildSetComponent } from './build-set/build-set.component';
 import { EditSongComponent } from './edit-song/edit-song.component';
 import { LeadSheetComponent } from './lead-sheet/lead-sheet.component';
@@ -24,7 +24,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SetContextMenuComponent } from './set-context-menu/set-context-menu.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthInterceptor } from './auth/auth-interceptor';
 
 @NgModule({
   declarations: [
